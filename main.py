e87f03612bd89cf3959d7e2d339ad0a78792970b
Marc Kohaupt <debuglevel@gmail.com>
2020-10-04 16:35:16 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import datetime
 
-def get_data():
+def retrieve_visitors():
     headers = {
         'Connection': 'keep-alive',
         'Accept': 'application/json, text/plain, */*',
@@ -16,25 +16,27 @@ def get_data():
         'Referer': 'https://www.stadtwerke-bamberg.de/baeder/bambados',
         'Accept-Language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
     }
-
     params = (
         ('organizationUnitIds', '30244'),
     )
-
     response = requests.get('https://api.ticos-systems.cloud/api/gates/counter', headers=headers, params=params)
+    visitor_data = response.json()
+    return visitor_data
 
-    visitorData = response.json()
-    currentVisitors = visitorData[0]["personCount"]
-    maximumSeats = visitorData[0]["maxPersonCount"]
-    freeSeats = maximumSeats - currentVisitors
-    currentDateTime = datetime.datetime.now()
+def get_visitors():
+    visitorData = retrieve_visitors()
 
-    return currentVisitors, maximumSeats, freeSeats, currentDateTime
+    current_visitors = visitorData[0]["personCount"]
+    maximum_seats = visitorData[0]["maxPersonCount"]
+    free_seats = maximum_seats - current_visitors
+    current_datetime = datetime.datetime.now()
+
+    return current_visitors, maximum_seats, free_seats, current_datetime
 
 def main():
-    currentVisitors, maximumSeats, freeSeats, timestamp = get_data()
-    iso8601timestamp = timestamp.replace(microsecond=0).isoformat()
-    print(f"{iso8601timestamp};{currentVisitors};{freeSeats};{maximumSeats}")
+    current_visitors, maximum_seats, free_seats, timestamp = get_visitors()
+    iso8601_timestamp = timestamp.replace(microsecond=0).isoformat()
+    print(f"{iso8601_timestamp};{current_visitors};{free_seats};{maximum_seats}")
 
 if __name__ == "__main__":
     main()
